8f641b2c95078099a54d66e94d901c654ec39bc7
axelMp <axel.krauth@methodpark.de>
2014-04-23 09:19:02 +0200

    Login as user,

--- grails-app/controllers/helloworld/UserController.groovy
+++ grails-app/controllers/helloworld/UserController.groovy
@@ -12,4 +12,15 @@ class UserController {
         def boards = curUser != null ? curUser.myTaskboards : null
         [user: curUser, taskboards: boards]
     }
+	
+	def login() {
+		if ( params.userlogin ) {
+			for (def user : userService.list()) {
+				if (user.myLogin == params.userlogin) {
+					session["user"] = user.id;
+					redirect(action:"show",id:user.id);	
+				}
+			}
+        }
+	}
 }
