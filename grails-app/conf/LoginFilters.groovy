a6825d85544d7c76efd728022d4e41ed889e243a
axelMp <axel.krauth@methodpark.de>
2014-04-24 09:20:05 +0200

    login link on start page

--- grails-app/conf/LoginFilters.groovy
+++ grails-app/conf/LoginFilters.groovy
@@ -2,7 +2,7 @@ class LoginFilters {
    def filters = {
         loginCheck(controller: '*') {
            before = {
-              if (!controllerName.equals('user') && !actionName.equals('login') && session["user"] == null) {			  
+            if (controllerName != null && !controllerName.equals('user') && !actionName.equals('login') && !session.user) {			  
                   redirect(controller: 'user', action:'login')
                   return false
                }
