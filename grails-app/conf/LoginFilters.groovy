762bc6ebf7f012c396b4799f03dc5d7d5755e327
axelMp <axel.krauth@methodpark.de>
2014-04-27 22:25:29 +0200

    works on my machine

--- grails-app/conf/LoginFilters.groovy
+++ grails-app/conf/LoginFilters.groovy
@@ -2,7 +2,7 @@ class LoginFilters {
     def filters = {
         loginCheck(controller: '*') {
             before = {
-                if (controllerName != null && !controllerName.equals('user') && !actionName.equals('login') && !session.user) {
+                if (controllerName != null && !controllerName.equals('user') && !actionName.equals('login') && null == session.user) {
                     redirect(controller: 'user', action:'login')
                         return false
                 }
