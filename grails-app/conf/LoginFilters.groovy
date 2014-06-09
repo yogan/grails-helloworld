02d369617c6febb4a8fa4b2ae4eeb6158d6030e5
Frank Blendinger <fb@intoxicatedmind.net>
2014-06-09 22:37:24 +0200

    LoginFilters: do not redirect for assets ctrlr

--- grails-app/conf/LoginFilters.groovy
+++ grails-app/conf/LoginFilters.groovy
@@ -2,9 +2,20 @@ class LoginFilters {
     def filters = {
         loginCheck(controller: '*') {
             before = {
-                if (controllerName != null && !controllerName.equals('user') && !actionName.equals('login') && null == session.user) {
+                //println("---------------------------------------------")
+                //println("Request.requestURI: "+request.requestURI)
+                //println("Request.forwardURI: "+request.forwardURI)
+                //println("Controller: "+controllerName)
+                //println("ActionName: "+actionName)
+                //println("USER: "+session.user)
+
+                if (controllerName != null 
+                        && !controllerName.equals('user') 
+                        && !controllerName.equals('assets') 
+                        && !actionName.equals('login') 
+                        && null == session.user) {
                     redirect(controller: 'user', action:'login')
-                        return false
+                    return false
                 }
             }
         }
